14a7b87d9cd0768bdfe0a513b9ea5c3242c261bc
sergey_kovalenko <sergey_kovalenko2107@ukr.net>
2022-07-06 10:51:05 +0300

    New commit

--- src/com/company/FirstTask.java
+++ src/com/company/FirstTask.java
@@ -2,10 +2,17 @@ package com.company;
 
 public class FirstTask {
     public static void main(String[] args) {
-        int[] array = {14, 11, 99, 23, -24, 1, 2, 8, 0, -17, 22, 56, 90, 71, 63, -124, 45, -1};
+        int[] array = {16, 11, 99, 23, -24, 1, 2, 8, 0, -17, 22, 56, 90, 71, 63, -124, 45, -1, 13};
+        System.out.print("Четные числа: ");
         for (int i = 0; i < array.length; i++) {
             if (array[i] % 2 == 0){
-                System.out.println(array[i]);
+                System.out.print(array[i] + " ");
+            }
+        }
+        System.out.print("\nНечетные числа: ");
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0){
+                System.out.print(array[i] + " ");
             }
         }
     }
