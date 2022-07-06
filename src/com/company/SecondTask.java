61672b34b61edd7ee401d93482c6097cff239848
sergey_kovalenko <sergey_kovalenko2107@ukr.net>
2022-07-06 11:10:48 +0300

    New commit

--- src/com/company/SecondTask.java
+++ src/com/company/SecondTask.java
@@ -4,7 +4,13 @@ public class SecondTask {
     public static void main(String[] args) {
         int[] array = {16, 11, 99, 23, -24, 1, 2, 8, 0, -17, 22, 56, 90, 71, 63, -124, 45, -1, 13};
         for (int i = 0; i < array.length; i++) {
-            if (array[i] % 2 != 0) {
+            boolean isSimple = true;
+            for (int j = 2; j < array[i]; j++) {
+                if (array[i] % j == 0) {
+                    isSimple = false;
+                }
+            }
+            if (array[i] > 1 && isSimple) {
                 System.out.println(array[i]);
             }
         }
